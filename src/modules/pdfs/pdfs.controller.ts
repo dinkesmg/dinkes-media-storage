9673dca5e29ae11b41487a02616d7986d7db7a18
B-RunP <brianpurnama157@gmail.com>
2026-01-12 14:21:53 +0700

    ubah maksimal ukuran upload pdf

--- src/modules/pdfs/pdfs.controller.ts
+++ src/modules/pdfs/pdfs.controller.ts
@@ -36,7 +36,7 @@ export class PdfsController {
   async uploadPdf(
     @UploadedFile(
       new ParseFilePipe({
-        validators: [new MaxFileSizeValidator({ maxSize: 10 * 1024 * 1024 })],
+        validators: [new MaxFileSizeValidator({ maxSize: 3 * 1024 * 1024 })],
         exceptionFactory: () =>
           new BadRequestException({
             statusCode: 400,
